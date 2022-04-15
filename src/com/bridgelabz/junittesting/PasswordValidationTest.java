852908f6270036f18ebb674a1a28a13455f34ffe
unknown <SmithaRai.com>
2022-04-15 17:05:19 +0530

    Added : added all emailid samples

--- src/com/bridgelabz/junittesting/PasswordValidationTest.java
+++ src/com/bridgelabz/junittesting/PasswordValidationTest.java
@@ -40,5 +40,74 @@ public class PasswordValidationTest {
 			Assert.assertEquals(true , result);
 		
 		            }
+	 
+	 public void emailid_sample1_returntrue() {
+			
+			UserPasswordRegistration obj = new UserPasswordRegistration();
+			boolean result = obj.validate_emailid_samples("abc@yahoo.com,");
+			Assert.assertEquals(true , result);
+		
+		            }
+	 
+	 public void emailid_sample2_returntrue() {
+			
+			UserPasswordRegistration obj = new UserPasswordRegistration();
+			boolean result = obj.validate_emailid_samples("abc-100@yahoo.com,");
+			Assert.assertEquals(true , result);
+		
+		            }
+	 
+	 public void emailid_sample3_returntrue() {
+			
+			UserPasswordRegistration obj = new UserPasswordRegistration();
+			boolean result = obj.validate_emailid_samples("abc.100@yahoo.com");
+			Assert.assertEquals(true , result);
+		
+		            }
+	 
+	 public void emailid_sample4_returntrue() {
+			
+			UserPasswordRegistration obj = new UserPasswordRegistration();
+			boolean result = obj.validate_emailid_samples("abc111@abc.com,");
+			Assert.assertEquals(true , result);
+		
+		            }
+	 
+	 public void emailid_sample5_returntrue() {
+			
+			UserPasswordRegistration obj = new UserPasswordRegistration();
+			boolean result = obj.validate_emailid_samples("abc.100@abc.com.au");
+			Assert.assertEquals(true , result);
+		
+		            }
+	 
+	 public void emailid_sample6_returntrue() {
+			
+			UserPasswordRegistration obj = new UserPasswordRegistration();
+			boolean result = obj.validate_emailid_samples("abc@1.com,");
+			Assert.assertEquals(true , result);
+		
+		            }
+	 
+	 public void emailid_sample7_returntrue() {
+			
+			UserPasswordRegistration obj = new UserPasswordRegistration();
+			boolean result = obj.validate_emailid_samples("abc@gmail.com.com");
+			Assert.assertEquals(true , result);
+		
+		            }
+	 
+	 public void emailid_sample8_returntrue() {
+			
+			UserPasswordRegistration obj = new UserPasswordRegistration();
+			boolean result = obj.validate_emailid_samples("abc+100@gmail.com");
+			Assert.assertEquals(true , result);
+		
+		            }
+	 
+	 
+	 
+	 
+	 
 	
 }
--- src/com/bridgelabz/junittesting/UserPasswordRegistration.java
+++ src/com/bridgelabz/junittesting/UserPasswordRegistration.java
@@ -8,6 +8,7 @@ public class UserPasswordRegistration {
 	 private static final String PASSWORD_RULE2 = "^[a-zA-Z0-9_-]{8,}[A-Z]+$";
 	 private static final String PASSWORD_RULE3 = "^[a-zA-Z0-9_-]{8,}[A-Z]+[0-9]+$";
 	 private static final String PASSWORD_RULE4 = "^[a-zA-Z0-9_-]{8,}[A-Z]+[0-9]+[! @#&()]{1}$";
+	 private static final String EMAIID_SAMPLES = "^[a-zA-Z.+0-9_-]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-zA-Z,]{2,}[.,a-zA-Z]?$";
 	 
 	 public boolean validatepassword_rule1 (String pwd_rule1){
          
@@ -30,11 +31,20 @@ public boolean validatepassword_rule3 (String pwd_rule3){
  
         }
 
+
 public boolean validatepassword_rule4 (String pwd_rule4){
     
     Pattern pattern = Pattern.compile(PASSWORD_RULE4);
     return pattern.matcher(pwd_rule4).matches();
  
         }
+
+public boolean validate_emailid_samples (String email_samples){
+    
+    Pattern pattern = Pattern.compile(EMAIID_SAMPLES);
+    return pattern.matcher(email_samples).matches();
+    
+}
+
 }
 
